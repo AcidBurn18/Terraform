e37744f1214c7c1645aaecfc279140b9c1a0e525
Ansh <iansh2118@gmail.com>
2023-04-07 20:03:39 +0530

    Create vars.tf

---
+++ vars.tf
@@ -0,0 +1,15 @@
+variable "configuration_virtualmachine" {
+  default = [{
+    virtual_machine_resource_group_name="rg"
+    virtual_machine_name="default"
+    virtual_machine_location="eastus"
+    virtual_machine_count="2"
+    virtual_network_name="default"
+    virtual_machine_prefix="prefix"
+    address_space=["10.0.0.0/16"]
+    address_prefix=["10.0.0.0/24"]
+    subnet_name="default-subnet"
+    virtual_machine_vm_size="Standard_DS1_v2"
+    os_disk_name ="default"
+  }]
+}
